f97c11e5b66bb4c50c116bb3a16c63525ed04b15
MichaelS0521 <michael@Zipcoders-MacBook-Pro-4.local>
2024-04-30 22:10:43 -0400

    Updated

---
+++ src/main/java/io/zipcoder/crudapp/controller/PersonController.java
@@ -0,0 +1,34 @@
+package io.zipcoder.crudapp.controller;
+
+import io.zipcoder.crudapp.entity.Person;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+
+    @
+    public Person createPerson(Person p) {
+        return null;
+    }
+
+    public Person getPerson(int id) {
+            return null;
+    }
+
+    public List<Person> getPersonList() {
+        return null;
+    }
+
+    public Person updatePerson(Person p) {
+        return null;
+    }
+
+    public void DeletePerson(int id) {
+
+    }
+
+}
---
+++ src/main/java/io/zipcoder/crudapp/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package io.zipcoder.crudapp.repository;
+
+import io.zipcoder.crudapp.entity.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+
+}
